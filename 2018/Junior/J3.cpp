267ed6d7a8908613b5b3b31a5d4474be9fd28071
HugoC1000 <121970065+HugoC1000@users.noreply.github.com>
2023-06-01 20:22:20 -0700

    Create J3.cpp

---
+++ 2018/Junior/J3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+
+    //Save stuff to distances
+    vector<int> distances(4);
+    for (int i = 0; i < 4; i++) {
+        cin >> distances[i];
+    }
+
+    vector<vector<int> > distanceTable(5, vector<int>(5));
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == j) {
+                distanceTable[i][j] = 0;
+            } else if (i < j) { //Sum right side of the thing
+                int sum = 0;
+                for (int k = i; k < j; k++) {
+                    sum += distances[k];
+                }
+                distanceTable[i][j] = sum;
+            } else { 
+                distanceTable[i][j] = distanceTable[j][i];
+            }
+        }
+    }
+
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j!= 4){
+            cout << distanceTable[i][j] << " ";
+            }else{
+                cout << distanceTable[i][j];
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
