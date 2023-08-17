495b29ee74da86c7b6733f579ba8e76afdc1fb23
HugoC1000 <121970065+HugoC1000@users.noreply.github.com>
2023-08-16 17:38:06 -0700

    Update 2023_J3_Python.py
    Added comments to explain the code

--- 2023/Junior/2023_J3_Python.py
+++ 2023/Junior/2023_J3_Python.py
@@ -1,34 +1,48 @@
+# Read the number of people attending the event
 N = int(input())
+
+# Initialize a list to store the availability of each person for each day
 overalist = []
+
+# Read the availability for each person and store it in overalist
 for a in range(N):
-  inp = input()
-  temp = []
-  for b in inp:
-    temp.append(b)
-  overalist.append(temp)
+    inp = input()
+    temp = []
+    for b in inp:
+        temp.append(b)
+    overalist.append(temp)
 
-countlist = [0,0,0,0,0]
+# Initialize a list to store the count of available people for each day
+countlist = [0, 0, 0, 0, 0]
 
+# Count the number of available people for each day
 for a in range(len(overalist)):
-  for b in range(5):
-    if overalist[a][b] == "Y":
-      countlist[b] += 1
+    for b in range(5):
+        if overalist[a][b] == "Y":
+            countlist[b] += 1
 
+# Initialize a list to store days with the maximum count of available people
 otherlist = []
+
+# Find the maximum count of available people
 c = max(countlist)
 
+# Check if there are multiple days with the same maximum count
 if countlist.count(c) > 1:
-  for a in range(len(countlist)):
-    if countlist[a] == c:
-      otherlist.append(a+1)
-  otherlist.sort()
-  for b in range(len(otherlist)):
-    if b != len(otherlist)-1:
-      print(otherlist[b], ",", sep = "",end = "")
-    else:
-      print(otherlist[b])
+    # Store days with the maximum count in otherlist
+    for a in range(len(countlist)):
+        if countlist[a] == c:
+            otherlist.append(a + 1)
+    otherlist.sort()
+    # Output the days with maximum count
+    for b in range(len(otherlist)):
+        if b != len(otherlist) - 1:
+            print(otherlist[b], ",", sep="", end="")
+        else:
+            print(otherlist[b])
 else:
-  for a in range(len(countlist)):
-    if countlist[a] == c:
-      print(a+1)
-      break
+    # Output the day with the highest count
+    for a in range(len(countlist)):
+        if countlist[a] == c:
+            print(a + 1)
+            break
