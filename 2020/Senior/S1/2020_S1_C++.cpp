782c58376a45da91857a0147c81ae83dc6c5f32f
HugoC1000 <121970065+HugoC1000@users.noreply.github.com>
2023-12-03 11:08:59 -0800

    Update 2020_S1_C++.cpp
    Rewrote the code to be more legible and added comments

--- 2020/Senior/S1/2020_S1_C++.cpp
+++ 2020/Senior/S1/2020_S1_C++.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <set>
+#include <map>
 #include <cmath>
-#include<iomanip>
-using std::cin;
-using std::cout;
-using namespace std;
-int main() {
-  
-  int numofinp;
-  cin >> numofinp;
-    vector<pair<int,int>>arr(numofinp);
-    for(int i = 0;i <numofinp;i++){
-      cin >> arr[i].first >> arr[i].second;
-    }
+int main(){
+  int N;
+  std::cin >> N;
+  std::map<double,double> map; //Create a map to create key value pairs of time to distance. 
+  std::set<double> seta; //Create a set to store and auto sort the keys.
+  for (int i = 0;i<N;i++){ //Input values into map and set
+    double temp1,temp2;
+    std::cin >> temp1 >>temp2;
+    map[temp1] = temp2;
+    seta.insert(temp1);
+  }
+  double speed = 0;
 
-  sort(arr.begin(),arr.end());
-  float max = 0;
-  for(int b = numofinp - 1; b >= 0;b--){
-    if(std::fabs(arr[b].second-arr[b-1].second)/(arr[b].first-arr[b-1].first) > max){
-      max = std::fabs((arr[b].second-arr[b-1].second))/(arr[b].first-arr[b-1].first);
-      
+  std::vector<double> setVector(seta.begin(), seta.end());  //Convert set into a vector
+  
+  for(int i =0;i<N-1;i++){ //Calculate the difference in time and position between each consecutive time stamps
+    double diffInTime  = setVector[i+1]-setVector[i];
+    double diffInPos = std::abs(map[setVector[i+1]]-map[setVector[i]]);
+    double newSpeed = diffInPos/diffInTime;
+    if (newSpeed > speed){ //Store the highest speed recorded so far. 
+      speed = newSpeed;
     }
   }
-  
-  cout << fixed << setprecision(1.0) <<max;
+  std::cout << speed; //Output
 }
