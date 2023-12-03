e7ab7a0a689f2ecb904c4ee3d16cf63cf583a426
HugoC1000 <121970065+HugoC1000@users.noreply.github.com>
2023-12-03 12:47:08 -0800

    Create 2015_J1_Python.py

---
+++ 2015/Junior/2015_J1_Python.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+
+if (a <= 2 and b < 18) or a < 2:
+    print("Before")
+elif (a == 2 and b > 18) or (a > 2):
+    print("After")
+elif a == 2 and b ==18:
+    print("Special")
